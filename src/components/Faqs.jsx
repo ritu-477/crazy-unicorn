cc91d06a552edb95514b31e193335420fcbcf0d5
Seema-476 <simmybishnoi29085@gmail.com>
2025-01-15 12:34:54 +0530

    changes

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Heading from '../common/Heading'
+import { FREQUENTLY_ASK_QUESTIONS_LIST } from '../utils/helper'
 
 const Faqs = () => {
+  const [openAccordion, setOpenAccordion] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setOpenAccordion(openAccordion === index ? null : index);
+  };
   return (
-    <div>Faqs</div>
+    <div className='bg-pink-300 pt-[106px] pb-[133px]'>
+      <div className='container'>
+        <Heading text={'FAQs'} ClassStyle={'text-center'}/>
+        {
+          FREQUENTLY_ASK_QUESTIONS_LIST.map((item, index) => (
+            <div key={index} className="max-w-[1187px] mx-auto">
+              <div className={`mb-[35px] ${openAccordion === index
+                ? "faq-gradient-border"
+                : "border-white border-opacity-10"
+                }`}>
+                <button
+                  onClick={() => toggleAccordion(index)}
+                  className={`font-normal text-custom-5xl leading-custom-7xl text-black flex justify-between w-full uppercase ${openAccordion === index ? "" : ""}`}
+                >
+                  {item.question}
+                  <img
+                    src="/assets/images/svg/faq-arrow.svg"
+                    alt={openAccordion === index ? "Collapse" : "Expand"}
+                    className={`w-6 h-[13.71px] transition-transform duration-500 transform ${openAccordion === index ? "rotate-180" : "rotate-0"
+                      }`} />
+                </button>
+                <div
+                  className={`transition-all duration-700 ease-in-out overflow-auto ${openAccordion === index
+                    ? "max-sm:h-[150px] opacity-100"
+                    : "max-h-0 opacity-0"
+                    }`}>
+                  <div className="transition-all duration-700 ease-in-out">
+                    {item.answer && <p className=" font-normal text-custom-2xl leading-custom-5xl text-black max-w-[831px] pt-[10px] pb-5 border-b border-b-black border-opacity-30">{item.answer}</p>}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
   )
 }
 
---
+++ src/utils/helper.js
@@ -0,0 +1,31 @@
+export const FREQUENTLY_ASK_QUESTIONS_LIST = [
+    {
+        question: "Q.1 Eu faucibus lorem nulla cras ?",
+        answer: "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+    {
+        question: "Q.2 Eu adipiscing ac felis mauris ?",
+        answer: "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+    {
+        question: "Q.3 Ridiculus amet gravida accumsan dis ?",
+        answer: "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+    {
+        question: "Q.4 Magna eu diam nec quis turpis tortor pulvinar ?",
+        answer: "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+    {
+        question: "Q.5 Morbi at cursus ut in augue ?",
+        answer: "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+    {
+        question: "Q.6 Cras pellentesque convallis semper dolor at vel ?",
+        answer: "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+    {
+        question: "Q.7 gravida maecenas amet tincidunt feugiat ?",
+        answer: "Pulvinar odio scelerisque suspendisse fringilla rutrum a. Vestibulum etiam egestas dictum tristique. Eu quis in eu nunc, aliquet in non elit. Dis tincidunt.",
+    },
+
+];
